62726bcb978312aa7d86afefb52736e1be30f933
ren <OliGaming122@gmail.com>
2022-02-10 11:28:30 +0100

    basic role command

--- src/main/java/com/ren/rank/Main.java
+++ src/main/java/com/ren/rank/Main.java
@@ -2,10 +2,12 @@ package com.ren.rank;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
+
 public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        
+        Objects.requireNonNull(getCommand("role")).setExecutor(new RoleCommand());
     }
 }
---
+++ src/main/java/com/ren/rank/RoleCommand.java
@@ -0,0 +1,14 @@
+package com.ren.rank;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class RoleCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        
+        return false;
+    }
+}
