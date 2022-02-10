f4ed00d6d542179b9cbe137da042ecdcdd854cef
ren <OliGaming122@gmail.com>
2022-02-10 11:26:27 +0100

    initial commit

---
+++ src/main/java/com/ren/rank/Main.java
@@ -0,0 +1,11 @@
+package com.ren.rank;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+    }
+}
